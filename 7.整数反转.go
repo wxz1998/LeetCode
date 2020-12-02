499e1ef2c38d9b4f64694c7867d665cd24729c9d
wxz1998 <wxz18737676272@163.com>
2020-12-02 17:42:25 +0800

    20201202

---
+++ 7.整数反转.go
@@ -0,0 +1,21 @@
+
+/*
+ * @lc app=leetcode.cn id=7 lang=golang
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+func reverse(x int) int {
+	var res int
+	for x != 0 {
+		if temp := int32(res); (temp*10)/10 != temp {
+			return 0
+		}
+		res = res*10 + x%10
+		x = x / 10
+	}
+	return res
+}
+
+// @lc code=end
