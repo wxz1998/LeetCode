a3acc08d843922de6a227daaf5729b91f5f68f40
wxz1998 <wxz18737676272@163.com>
2020-12-02 18:02:56 +0800

    20201202

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+ * @Author: zut.wxz
+ * @Date: 2020-12-02 18:01:38
+ * @LastEditors: zut.wxz
+ * @LastEditTime: 2020-12-02 18:02:16
+ * @Description:
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println()
+}
