d7eb75a405c05f15019aeada46fa2ca46293046b
wxz1998 <wxz18737676272@163.com>
2020-12-02 18:00:37 +0800

    20201202

--- 9.回文数.go
+++ 9.回文数.go
@@ -7,7 +7,20 @@
 
 // @lc code=start
 func isPalindrome(x int) bool {
-
+	if x < 0 {
+		return false
+	}
+	val := 0
+	temp := x
+	for x != 0 {
+		val = val*10 + x%10
+		x = x / 10
+	}
+	if val == temp {
+		return true
+	} else {
+		return false
+	}
 }
 
 // @lc code=end
